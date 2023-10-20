f2c88a54db48652ac67e02e89a85d6684e09be8f
Jan Steggemann <jan.steggemann@cern.ch>
2023-10-20 12:09:17 +0200

    Shorten, streamline

--- digi_to_ml.py
+++ digi_to_ml.py
@@ -1,3 +1,8 @@
+''' Script to create npy files as input for ML training.
+Adapted from https://github.com/golovart/Napoli_emulsion/blob/main/SND/neutrino_ml/data_process/hit_sandbox.py
+'''
+
+
 ## run example
 # python -i hit_sandbox.py -mc /eos/experiment/sndlhc/MonteCarlo/Neutrinos/Genie/sndlhc_13TeV_down_volTarget_100fb-1_SNDG18_02a_01_000 -p 137 -t neutrino
 
@@ -8,49 +13,31 @@
 ## event metadata format: [Startz, MCTrack[0].GetPdgCode(), MCTrack[1].GetPdgCode()]
 
 
-import ROOT
-import os,sys,subprocess,atexit
-import rootUtils as ut
-from array import array
-import shipunit as u
-import SndlhcMuonReco
-import json
-from rootpyPickler import Unpickler
-import time
-from XRootD import client
-
+import os #, atexit
 from datetime import datetime
-from pathlib import Path
 from tqdm import tqdm
 import numpy as np
-import matplotlib.pyplot as plt
-# import matplotlib as mpl
+import ROOT
 
 
-def pyExit():
-       "unfortunately need as bypassing an issue related to use xrootd"
-       os.system('kill '+str(os.getpid()))
-atexit.register(pyExit)
-
-h = {}
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument("-mc", "--inputMCDir", dest="inputMCDir", help="Monte Carlo input directory", required=False, default="/eos/experiment/sndlhc/MonteCarlo/Neutrinos/Genie/sndlhc_13TeV_down_volMuFilter_20fb-1_SNDG18_02a_01_000/")
+parser.add_argument("-mc", "--inputmc_dir", dest="inputmc_dir", help="Monte Carlo input directory", required=False, default="/eos/experiment/sndlhc/MonteCarlo/Neutrinos/Genie/sndlhc_13TeV_down_volMuFilter_20fb-1_SNDG18_02a_01_000/")
 parser.add_argument("-p", "--partition", dest="part", help="number of starting partition", default=0)
-parser.add_argument("-ne", "--nEvents", dest="nEvents", help="number of events to process", default=3000001)
 parser.add_argument("-o", "--outPath", dest="outPath", help="output directory", required=False,
                     default="/afs/cern.ch/user/s/steggema/work/snd/data/")
 parser.add_argument("-t", "--type", dest="etype", help="event type to select", default='neutrino')
 
 options = parser.parse_args()
-import SndlhcGeo
+
+import SndlhcGeo # import takes some time so putting it after the arg parser
 
 # find geofile in the MC dir
-MCDir = os.path.join(options.inputMCDir, str(options.part)) # '/'.join(options.inputMCFile.split('/')[:-1])
+mc_dir = os.path.join(options.inputmc_dir, str(options.part)) # '/'.join(options.inputMCFile.split('/')[:-1])
 geo_path = None
-for name in os.listdir(MCDir):
+for name in os.listdir(mc_dir):
     if 'geofile' in name:
-        geo_path = os.path.join(MCDir, name)
+        geo_path = os.path.join(mc_dir, name)
 if geo_path is None:
     raise RuntimeError("no geofile found in the MC directory")
 
@@ -62,36 +49,24 @@ Scifi = geo.modules['Scifi']
 Mufi = geo.modules['MuFilter']
 nav = ROOT.gGeoManager.GetCurrentNavigator()
 
-## Processing the geometry
-detSize = {}
-si = geo.snd_geo.Scifi
-detSize[0] = [si.channel_width, si.channel_width, si.scifimat_z]
-mi = geo.snd_geo.MuFilter
-detSize[1] =[mi.VetoBarX/2, mi.VetoBarY/2, mi.VetoBarZ/2]
-detSize[2] =[mi.UpstreamBarX/2, mi.UpstreamBarY/2, mi.UpstreamBarZ/2]
-detSize[3] =[mi.DownstreamBarX_ver/2, mi.DownstreamBarY/2, mi.DownstreamBarZ/2]
-
-
-print('\n\n-- Getting detector sizes --')
-print(detSize)
 A, B = ROOT.TVector3(), ROOT.TVector3()
 proc_start_time = datetime.now()
 
-## For MonteCarlo INPUT FILE
 tchain = ROOT.TChain("cbmsim")
 
-MCFile_path = None
-for name in os.listdir(MCDir):
+mc_file_path = None
+for name in os.listdir(mc_dir):
     if name.endswith('digCPP.root'):
-        MCFile_path = os.path.join(MCDir, name)
-if MCFile_path is None:
+        mc_file_path = os.path.join(mc_dir, name)
+if mc_file_path is None:
     raise RuntimeError("no MC digi file found in the MC directory")
 
-tchain.Add(MCFile_path)  
+tchain.Add(mc_file_path)  
 
 ## OUTPUT FILE
-out_path = os.path.join(options.outPath, options.etype, *MCDir.split('/')[-2:])
+out_path = os.path.join(options.outPath, options.etype, *mc_dir.split('/')[-2:])
 if not os.path.exists(out_path):
+    print('Creating output directory:', out_path)
     os.makedirs(out_path)
 
 # nchan = {'scifi':1536, 'us':10, 'ds':60}
@@ -109,10 +84,6 @@ event_meta = np.zeros((N_events, 3))
 # - 7 detector type (0: scifi, 1: us, 2: ds)
 hitmap = np.zeros((N_events, 8, 1000), dtype=np.float32) # use uint8?
 
-# hitmap['scifi'] = np.zeros((N_events, 2, nplane['scifi'], nchan['scifi']), dtype=bool) # use uint8?
-# hitmap['us'] = np.zeros((N_events, 2, nplane['us'], nchan['us']), dtype=bool)
-# hitmap['ds'] = np.zeros((N_events, 2, nplane['ds'], nchan['ds']), dtype=bool)
-
 scifi_depth = []
 for i_event, event in tqdm(enumerate(tchain), total=N_events):
     event_pdg0 = event.MCTrack[0].GetPdgCode()
@@ -122,10 +93,6 @@ for i_event, event in tqdm(enumerate(tchain), total=N_events):
     if options.etype=='neutron':
         if not (event.MCTrack[0].GetPdgCode()==2112): continue
         #print('event ', i_event,' track0 type: ', event.MCTrack[0].GetPdgCode())
-    #digi_hits = []
-    #if event.FindBranch("Digi_ScifiHits"): digi_hits.append(event.Digi_ScifiHits)
-    #if event.FindBranch("Digi_MuFilterHits"): digi_hits.append(event.Digi_MuFilterHits)
-    #if event.FindBranch("Digi_MuFilterHit"): digi_hits.append(event.Digi_MuFilterHit)
     
     event_meta[i_event] = (event.MCTrack[0].GetStartZ(), event.MCTrack[0].GetPdgCode(), event.MCTrack[1].GetPdgCode())
     
@@ -135,9 +102,11 @@ for i_event, event in tqdm(enumerate(tchain), total=N_events):
         # if not aHit.isValid(): continue
         detID = aHit.GetDetectorID()
         vert = aHit.isVertical()
-        # geo.modules['Scifi'].GetSiPMPosition(detID, A, B) # https://github.com/SND-LHC/sndsw/blob/a3ff0d0c4dfd8af5b12dbea31f9fb5b70f3c3ce9/shipLHC/Scifi.cxx#L557
-        geo.modules['Scifi'].GetPosition(detID, A, B)
-        # Note should also check https://github.com/SND-LHC/sndsw/blob/a3ff0d0c4dfd8af5b12dbea31f9fb5b70f3c3ce9/shipLHC/Scifi.cxx#L494
+        geo.modules['Scifi'].GetSiPMPosition(detID, A, B) # https://github.com/SND-LHC/sndsw/blob/a3ff0d0c4dfd8af5b12dbea31f9fb5b70f3c3ce9/shipLHC/Scifi.cxx#L557
+        # geo.modules['Scifi'].GetPosition(detID, A, B) # https://github.com/SND-LHC/sndsw/blob/a3ff0d0c4dfd8af5b12dbea31f9fb5b70f3c3ce9/shipLHC/Scifi.cxx#L494
+        # the second gives errors of type 
+        # Error in <TGeoNavigator::cd>: Path /cave_1/Detector_0/volTarget_1/ScifiVolume1_1000000/ScifiHorPlaneVol1_1000000/HorMatVolume_1000000/FiberVolume_1010000 not valid
+        # so we go with the first (which are also fibre positions so fine)
         scifi_depth = np.append(scifi_depth, A.z())
 
         hitmap[i_event, 0, i_hit] = vert
@@ -158,6 +127,7 @@ for i_event, event in tqdm(enumerate(tchain), total=N_events):
 
         geo.modules['MuFilter'].GetPosition(detID, A, B)
 
+        # The following gives the subsystem number (2: us, 3: ds)
         n_sys = detID // 10000
 
         hitmap[i_event, 0, i_hit] = vert
@@ -169,69 +139,18 @@ for i_event, event in tqdm(enumerate(tchain), total=N_events):
         hitmap[i_event, 6, i_hit] = B.z()
         hitmap[i_event, 7, i_hit] = n_sys - 1 # 0: scifi, 1: us, 2: ds
 
-    # print('\n'*3)
-    # if i_event>10: break
 print(np.unique(np.around(scifi_depth, decimals=0), return_counts=True))
 
-# Scifi coords
-detID = 1000000
-geo.modules['Scifi'].GetSiPMPosition(detID, A, B)
-print('\nScifi 0 horiz:', np.around(A, decimals=0), np.around(B, decimals=0))
-geo.modules['Scifi'].GetSiPMPosition(detID, A, B)
-print('Scifi 0 horiz end:', np.around(A, decimals=0), np.around(B, decimals=0))
-
-detID = 1023127
-geo.modules['Scifi'].GetSiPMPosition(detID, A, B)
-print('Scifi 0 horiz end:', np.around(A, decimals=0), np.around(B, decimals=0))
-geo.modules['Scifi'].GetPosition(detID, A, B)
-print('Scifi 0 horiz end:', np.around(A, decimals=0), np.around(B, decimals=0))
-
-detID = 1100000
-geo.modules['Scifi'].GetSiPMPosition(detID, A, B)
-print('\nScifi 0 vert:', np.around(A, decimals=0), np.around(B, decimals=0))
-geo.modules['Scifi'].GetPosition(detID, A, B)
-print('Scifi 0 vert end:', np.around(A, decimals=0), np.around(B, decimals=0))
-
-detID = 1123127
-geo.modules['Scifi'].GetSiPMPosition(detID, A, B)
-print('Scifi 0 vert end:', np.around(A, decimals=0), np.around(B, decimals=0))
-geo.modules['Scifi'].GetPosition(detID, A, B)
-print('Scifi 0 vert end:', np.around(A, decimals=0), np.around(B, decimals=0))
-
-detID = 20000
-geo.modules['MuFilter'].GetPosition(detID, A, B)
-print('\nUpStream 0 horiz:', np.around(A, decimals=0), np.around(B, decimals=0))
-detID = 20009
-geo.modules['MuFilter'].GetPosition(detID, A, B)
-print('UpStream 0 horiz end:', np.around(A, decimals=0), np.around(B, decimals=0))
-
-detID = 30000
-geo.modules['MuFilter'].GetPosition(detID, A, B)
-print('\nDownStream 0 horiz:', np.around(A, decimals=0), np.around(B, decimals=0))
-
-detID = 30060
-geo.modules['MuFilter'].GetPosition(detID, A, B)
-print('\nDownStream 0 vert:', np.around(A, decimals=0), np.around(B, decimals=0))
+debug = False
+if debug:
+    for det_id in [1000000, 1100000, 1023127, 1123127, 20009, 30060, 20000, 30000]:
+        det = 'Scifi' if det_id > 1000000 else 'MuFilter'
+        geo.modules[det].GetSiPMPosition(detID, A, B)
+        print(f'\n{det} {detID} SiPM:', np.around(A, decimals=0), np.around(B, decimals=0))
+        if det == 'Scifi':
+            geo.modules[det].GetPosition(detID, A, B)
+            print(f'{det} {detID} horiz pos:', np.around(A, decimals=0), np.around(B, decimals=0))
 
 
-
-
-np.save(out_path+name+'.npy', hitmap)
-np.save(out_path+'event_metadata.npy', event_meta)
-
-'''
-hit_tot = {}
-hit_tot['scifi'] = np.sum(hitmap['scifi'], axis=0, dtype=int)
-tmp_scifi = np.zeros((2, nplane['scifi'], 12))
-for i in range(12):
-    tmp_scifi[:, :, i] = hit_tot['scifi'][..., 128*i:128*(i+1)].sum(axis=-1)
-hit_tot['scifi'] = tmp_scifi
-hit_tot['us'] = np.sum(hitmap['us'], axis=0, dtype=int)
-hit_tot['ds'] = np.sum(hitmap['ds'], axis=0, dtype=int)
-
-for name, hitmap in hit_tot.items():
-    for vert in range(2):
-        plt.imshow(hitmap[vert].T)
-        plt.title(name+' '+('vert' if vert else 'horiz'))
-        plt.show()
-'''
+np.save(os.path.join(out_path, 'hits.npy'), hitmap)
+np.save(os.path.join(out_path, 'event_metadata.npy'), event_meta)
