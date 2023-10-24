44a5e53fb32cfcfda21c246e96b419798798a1bc
Jan Steggemann <jan.steggemann@cern.ch>
2023-10-24 12:31:34 +0200

    Make it work on lxplus

--- condor/merge_condor.py
+++ condor/merge_condor.py
@@ -6,8 +6,9 @@ import glob
 
 import argparse
 parser = argparse.ArgumentParser(description='Merge npz files')
-parser.add_argument("-mc", "--input_files", dest="input_files", help="Location of input npz files with wildcard", required=False, default='/afs/cern.ch/user/s/steggema/work/snd/data/neutrino/sndlhc_13TeV_down_volTarget_100fb-1_SNDG18_02a_01_000/*/*npz')
-parser.add_argument('-o', '--output_file', dest='output_file', help='Output file name', required=False, default='output.npz')
+parser.add_argument("-i", "--input_files", dest="input_files", help="Location of input npz files with wildcard", required=False, default='/afs/cern.ch/user/s/steggema/work/snd/data/neutrino/sndlhc_13TeV_down_volTarget_100fb-1_SNDG18_02a_01_000/*/*npz')
+parser.add_argument('-o', '--output_file', dest='output_file', help='Output file name', required=False, default='output')
+parser.add_argument('-n', '--n_files_merge', dest='n_merge', type=int, help='Number of input files for one output file', required=False, default=20)
 
 args = parser.parse_args()
 
@@ -17,14 +18,22 @@ output_file = args.output_file
 print('Input files: ', input_files)
 print('Output file: ', output_file)
 
-# Load the first file
-data = np.load(input_files[0])['arr_0']
-# Loop over the rest
-for input_file in input_files[1:]:
-    with np.load(input_file) as data_in:
-        print('Merging ', input_file)
-        data = np.concatenate(data, data_in['arr_0'])
+j = 0
 
-# Save the output
-print('Saving to ', output_file)
-np.savez_compressed(output_file, data)
+# Loop over the rest
+for i, input_file in enumerate(input_files):
+    if i % args.n_merge == 0:
+        data = np.load(input_file)['arr_0']
+    else:
+        with np.load(input_file) as data_in:
+            print('Merging ', input_file)
+            data = np.concatenate([data, data_in['arr_0']])
+    print(i+1, type(i+1), (i + 1) % args.n_merge, args.n_merge, type(args.n_merge))
+    if (i + 1) % args.n_merge == 0 or i == len(input_files) - 1:
+        # Save the output
+        out_name = f'{output_file}_{j}.npz'
+        print('Saving to ', out_name)
+        np.savez_compressed(out_name, data)
+        j += 1
+
+
