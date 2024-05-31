c62244d485e9fbdf781d05e887b6214a52d26a61
Jan Steggemann <jan.steggemann@cern.ch>
2024-05-31 13:43:12 +0200

    Better deail with non-contiguous run list

--- condor/create_condor_param_list.py
+++ condor/create_condor_param_list.py
@@ -33,15 +33,15 @@ with open('paramlist_neutrinoMC.txt', 'w') as out_file:
         end += partitions_to_merge
 
 # 2023 data
-partitions_to_merge = 50
+partitions_to_merge = 20
 with open('paramlist_data_2023.txt', 'w') as out_file:
     sample_dir = os.path.join('/eos/experiment/sndlhc/convertedData/physics/2023_reprocess/')
     partitions = os.listdir(sample_dir)
-    partitions = [int(i[-4:]) for i in partitions if i.startswith('run')]
+    partitions = sorted([int(i[-4:]) for i in partitions if i.startswith('run')])
     max_partition = max(partitions)
     min_partition = min(partitions)
-    start, end = min_partition, partitions_to_merge + min_partition
-    while start <= max_partition:
-        out_file.write(f"neutrino {start} {sample_dir} {end-1} 1\n")
+    start, end = 0, partitions_to_merge
+    while start <= len(partitions):
+        out_file.write(f"neutrino {partitions[start]} {sample_dir} {partitions[end-1] if end - 1 < len(partitions) else partitions[-1]} 1\n")
         start += partitions_to_merge
         end += partitions_to_merge
