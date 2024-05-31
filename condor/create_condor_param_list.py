6421c48b13a19be4d9ae8ab271a1c39fd8f3d85a
Jan Steggemann <jan.steggemann@cern.ch>
2024-05-31 11:09:53 +0200

    Add data processing

--- condor/create_condor_param_list.py
+++ condor/create_condor_param_list.py
@@ -1,7 +1,8 @@
 import os
+
+# Neutrons
 base_dir = '/eos/experiment/sndlhc/users/marssnd/PGsim/neutrons/'
 partitions_to_merge = 150
-
 with open('paramlist_neutronMC.txt', 'w') as out_file:
     for sample in os.listdir(base_dir):
         if 'tgtarea' in sample:
@@ -10,13 +11,14 @@ with open('paramlist_neutronMC.txt', 'w') as out_file:
             partitions = [int(i) for i in partitions]
             max_partition = max(partitions)
             min_partition = min(partitions)
-            start, end = min_partition, partitions_to_merge + min_partition
-            while end <= max_partition:
+            start, end = min_partition, partitions_to_merge + min_partition - 1
+            while start <= max_partition:
                 out_file.write(f"neutron {start} {sample_dir} {end}\n")
                 start += partitions_to_merge
                 end += partitions_to_merge
                 break # only first file
 
+# Neutrinos
 partitions_to_merge = 1
 with open('paramlist_neutrinoMC.txt', 'w') as out_file:
     sample_dir = os.path.join('/eos/experiment/sndlhc/MonteCarlo/Neutrinos/Genie/sndlhc_13TeV_down_volTarget_100fb-1_SNDG18_02a_01_000')
@@ -24,9 +26,22 @@ with open('paramlist_neutrinoMC.txt', 'w') as out_file:
     partitions = [int(i) for i in partitions]
     max_partition = max(partitions)
     min_partition = min(partitions)
-    start, end = min_partition, partitions_to_merge + min_partition
-    while end <= max_partition:
+    start, end = min_partition, partitions_to_merge + min_partition - 1
+    while start <= max_partition:
         out_file.write(f"neutrino {start} {sample_dir} {end-1}\n")
         start += partitions_to_merge
         end += partitions_to_merge
 
+# 2023 data
+partitions_to_merge = 50
+with open('paramlist_data_2023.txt', 'w') as out_file:
+    sample_dir = os.path.join('/eos/experiment/sndlhc/convertedData/physics/2023_reprocess/')
+    partitions = os.listdir(sample_dir)
+    partitions = [int(i[-4:]) for i in partitions if i.startswith('run')]
+    max_partition = max(partitions)
+    min_partition = min(partitions)
+    start, end = min_partition, partitions_to_merge + min_partition - 1
+    while start <= max_partition:
+        out_file.write(f"neutrino {start} {sample_dir} {end-1}\n")
+        start += partitions_to_merge
+        end += partitions_to_merge
